368ed506573baa77e41aff7baae3505d5862f6e7
vikaswilson1980-creator <vikaswilson1980@gmail.com>
2025-10-25 10:25:43 +0530

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age:"))
+
+if(age>=10 and age<=20):
+    print("You are between the age of 10 to 20")
+else:
+    if(age>=20):
+        print("You are above 20")
+    else:
+        print("You are below 10")
